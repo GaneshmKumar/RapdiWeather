95aacbc4f6c7bf0bc37aca5fc83b5dfb2511e2e4
GaneshmKumar <ganeshkumar.m1996@gmail.com>
2016-06-13 00:10:10 +0530

    Updated

--- rapid_weather.js
+++ rapid_weather.js
@@ -1,69 +1,31 @@
-lat = ""
-lon = ""
-city = ""
-temperature = ""
-ftemp = 0
-function convert() {
-     
-  var unit = $("#unit").text();
-  if(unit === "FARENHEIT")
-    {
-      $("#temperature").text(ftemp+" Farenheit")
-      $("#unit").text("CELCIUS");
-   }
-  else if(unit === "CELCIUS")
-    {
-      $("#temperature").text(temperature+" Celcius");
-      $("#unit").text("FARENHEIT");
-    }
-}
+$(document).ready(function(){
+  celcius = 1;
+  $("#temperature").click(function(){
+    if(celcius == 1)
+      {
+      document.getElementById("temperature").innerHTML = faren + " &#176;F";
+        celcius = 0;
+      }
+    else
+      {
+        document.getElementById("temperature").innerHTML = temp + " &#176;C";
+        celcius = 1
+      }
 
-$(document).ready(function() {
-  if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    
-    
-  $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true",function(json) {
-    city =  JSON.stringify(json["results"][5].address_components[0].long_name);
-  city = city.replace(/[^\w\s]/gi, '');
-    $("#city").text(city);
-   $("#latitude").text("Latitude: "+lat+" <---> "+"Longitude: "+lon);
-    
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=4a87519db6e540f39c6651e9e9f89a8b", function(json) {
-  temperature = JSON.stringify(json["main"].temp);
-  temperature = temperature - 273;
-  temperature = temperature.toFixed(1);
-  ftemp = parseInt(temperature) * 1.8 + 32; 
-  $("#temperature").text(temperature + " Celcius");
-
- 
- var img = JSON.stringify(json["weather"][0].icon);
-  
-  
-  var desired = img.replace(/[^\w\s]/gi, '');
-  
-  img = desired + ".png";
-  
-document.getElementById("icon").src="http://openweathermap.org/img/w/"+img;
- 
-});
- });
-  
   });
-
-  }
- 
-  
-}); 
-
-
-
-
-
-
-
-
-
-
+  $.getJSON("http://ipinfo.io/json").success(function(data) {
+    city = data.city;
+    country = data.country;
+    document.getElementById("city").innerHTML = city + ', ' + country;
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=061f24cf3cde2f60644a8240302983f2").success(function(data) {
+      temp = data.main["temp"];
+      faren = Math.round(temp * (9/5) + 32);
+      //alert(faren)
+      icon = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+      climate = data.weather[0].description;
+      $("#icon").attr("src", icon);
+      document.getElementById("temperature").innerHTML = temp + " &#176;C";
+      document.getElementById("climate").innerHTML = climate;
+    });
+  });
+});
